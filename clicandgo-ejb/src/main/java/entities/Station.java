9b3883bbf55019652816853463a7db2f17503bb7
esprit <esprit@pc-esprit.local>
2015-11-08 01:14:09 +0100

    Signed-off-by: esprit <esprit@pc-esprit.local>

--- clicandgo-ejb/src/main/java/entities/Station.java
+++ clicandgo-ejb/src/main/java/entities/Station.java
@@ -18,9 +18,31 @@ public class Station implements Serializable {
 
 	private Integer stationId;
 	private String name;
+	private Integer reference;
 	private static final long serialVersionUID = 1L;
 
 	private List<StationLine> stationLines;
+
+	public Station(){}
+	
+	
+	public Station(String name, Integer reference) {
+		super();
+		this.name = name;
+		this.reference = reference;
+	}
+	
+	
+
+	public Station(String name, Integer reference,
+			List<StationLine> stationLines) {
+		super();
+		this.name = name;
+		this.reference = reference;
+		this.stationLines = stationLines;
+	}
+
+
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -52,6 +74,14 @@ public class Station implements Serializable {
 	@Override
 	public String toString() {
 		return "Station [stationId=" + stationId + ", name=" + name + "]";
+	}
+
+	public Integer getReference() {
+		return reference;
+	}
+
+	public void setReference(Integer reference) {
+		this.reference = reference;
 	}
 
 }
