07b389a10fc11efa3134d940c9c8d9782d3a49f2
yosrgouider <=yosr.gouider@esprit.tn>
2015-11-10 12:10:18 +0100

    Signed-off-by: yosrgouider <=yosr.gouider@esprit.tn>

--- clicandgo-client/src/main/java/Dijkstra/Mapy.java
+++ clicandgo-client/src/main/java/Dijkstra/Mapy.java
@@ -1,5 +1,6 @@
 package Dijkstra;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -16,10 +17,11 @@ public class Mapy extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D gr = (Graphics2D) g;
-		//gr.setColor(Color.red);
+	//gr.setColor(Color.red);
 		List<Line> lines = LineServicesDelegate.doFindAllLines();
 		
 		for (Line l : lines) {
+			//setBackground(getBackground());
 			
 			List<Station> stations = StationLineManagementDelegate.doFindAllStationsByLineId(l.getLineId());
 			
@@ -31,15 +33,17 @@ public class Mapy extends JPanel {
 				
 				if (station.getX() != null && station.getY() != null) {
 					gr.setColor(Color.red);
-					gr.fillOval(station.getX(), station.getY(), 10, 10);
+					gr.fillOval(station.getX(), station.getY(), 13, 13);
 					gr.setColor(Color.BLUE);
+		
 					gr.drawString(station.getName(), station.getX(), station.getY()-10);
-
+                     
 				}
 
 			}
 			for (int i = 0; i < stations.size()-1; i++) {
 				gr.setColor(Color.black);
+				gr.setStroke(new BasicStroke( (float) 1.5 ));
 				gr.drawLine(stations.get(i).getX()+5, stations.get(i).getY()+5,
 						stations.get(i+1).getX()+5, stations.get(i+1).getY()+5);
 			}
